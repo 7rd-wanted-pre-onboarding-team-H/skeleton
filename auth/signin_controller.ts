82f17434ed4bc0c83541c9f679b21abdb507688f
jhmin95 <tesla52@naver.com>
2023-10-28 21:09:15 +0900

    fix: jwt 의 iat, exp 값을 제대로 설정, 쿠키의 maxAge 속성을 설정해 쿠키가 만료되면 삭제되도록 설정

--- auth/signin_controller.ts
+++ auth/signin_controller.ts
@@ -4,15 +4,13 @@ import { getUserByName } from "./signin_data.ts";
 import { DB } from "../types.ts";
 import { Kysely } from "kysely";
 import { sign } from 'hono/jwt'
-
-import { config } from "https://deno.land/x/dotenv/mod.ts"
+import { setCookie } from "hono/cookie";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 export const signInController = (db: Kysely<DB>) => {
     return new OpenAPIHono().openapi(signInUser, async (c) => {
         const { name, password } = c.req.valid("json")
         
-        const envFileObject = config()
-
         console.log(name, password)
         const user = await getUserByName(db, name);
         if (!user) {
@@ -23,14 +21,13 @@ export const signInController = (db: Kysely<DB>) => {
             return c.jsonT({ error: "로그인 실패" }, 401);
         }
         const payload = {
-            iat: Date.now(),
+            iat: Math.floor(Date.now() / 1000), // 1000 으로 나눠서 초 단위로 만들어줌
             sub: 'wanted social-feed user',
-            // 1 day
-            exp: Date.now() + 1000 * 60 * 60 * 24 * 1,
+            exp: Math.floor((Date.now() / 1000)) + ( 60 * 60 * 24 * 1 ),  // 1 day
             userId: user.id,
         };
-        const token = await sign(payload, envFileObject.SECRET_KEY);
-        c.res.headers.set("Set-Cookie", `access_token=${token};`)
+        const token = await sign(payload, config().SECRET_KEY);
+        setCookie(c, 'access-token', token, { path: '/', maxAge: 60 * 60 * 24 * 1})
         return c.text("로그인 성공");
     });
 }
