60c7398ab7dc5f1e955706882478781d949f5d82
scarf <greenscarf005@gmail.com>
2023-10-28 15:57:25 +0900

    refactor: early return 사용
    Co-authored-by: 심규철 <sphy1597@naver.com>

--- likes/likes_controller.ts
+++ likes/likes_controller.ts
@@ -8,29 +8,25 @@ import endpoints from "../endpoint.json" with { type: "json" }
 
 export const likesController = (db: Kysely<DB>) =>
 	new OpenAPIHono().openapi(likesRoute, async (c) => {
-		try {
-			const { id } = c.req.valid("param")
+		const { id } = c.req.valid("param")
 
-			// getPostlikes 함수로 좋아요 정보 가져오기
-			const likes = await getPostLikes(db, id)
+		// getPostlikes 함수로 좋아요 정보 가져오기
+		const likes = await getPostLikes(db, id)
 
-			if (likes) {
-				const endpoint = endpoints[likes.type]
-				const url = `${endpoint}/likes/${id}`
-
-				const response = await fetch(url, { method: "POST" })
-				await response.body?.cancel()
-                const updatedLikes = likes.like_count + 1
-                await updateLikes(db, id, updatedLikes)
-                const message = response.status === 200 ? '진짜' : '가짜'
-
-                return c.jsonT({ message: `좋아요 ${message} 성공`, like_count: updatedLikes })
-			} else {
-				// 게시글 없음
-				return c.jsonT({ error: "게시물이 없습니다" }, 404)
-			}
-		} catch (error) {
-			console.error("An error occurred:", error)
-			return c.jsonT({ error: "Not Found 4" }, 404)
+		if (!likes) {
+			return c.jsonT({ error: "게시물이 없습니다" }, 404)
 		}
+
+		const endpoint = endpoints[likes.type]
+		const url = `${endpoint}/likes/${id}`
+
+		const response = await fetch(url, { method: "POST" })
+		await response.body?.cancel()
+
+		const updatedLikes = likes.like_count + 1
+		const message = response.status === 200 ? "진짜" : "가짜"
+
+		await updateLikes(db, id, updatedLikes)
+
+		return c.jsonT({ message: `좋아요 ${message} 성공`, like_count: updatedLikes })
 	})
