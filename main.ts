1732aa9406464dfe3feda5fc732e5872650f0798
jhmin95 <tesla52@naver.com>
2023-10-28 21:10:32 +0900

    feat: /hello 경로에서 미들웨어에서 jwt 토큰이 있어야 가능하도록 설정

--- main.ts
+++ main.ts
@@ -7,18 +7,19 @@ import { postingController } from "./posting/mod.ts"
 import { likesController } from "./likes/mod.ts"
 import { signInController } from "./auth/signin_controller.ts"
 import { kyselyFrom } from "./kysely_from.ts"
+import { jwt } from "hono/jwt"
+import { config } from "https://deno.land/x/dotenv/mod.ts"
 
 const db = kyselyFrom(Deno.args[0] ?? "test.db")
-
 const app = new OpenAPIHono()
 	.use("*", logger(), prettyJSON())
+	.use("/hello/*", jwt( { secret: config().SECRET_KEY, cookie: 'access-token' }))
 	.route("", helloController())
 	.route("", postingController(db))
 	.route("", likesController(db))
 	.route("", signInController(db))
 	
 serveOpenapi(app as OpenAPIHono)
-
 Deno.serve({ port: 3000 }, app.fetch)
 
 app.showRoutes()
