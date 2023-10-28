4928fb318505dd6900b928179dbde606c716faae
jhmin95 <tesla52@naver.com>
2023-10-28 21:11:38 +0900

    feat: jwt 미들웨어를 통과한 hello controller 에서 jwt payload 를 받아올 수 있도록 함.

--- hello/hello_controller.ts
+++ hello/hello_controller.ts
@@ -4,6 +4,7 @@ import { helloRoute } from "./hello_routes.ts"
 export const helloController = () =>
 	new OpenAPIHono().openapi(helloRoute, (c) => {
 		const { name } = c.req.valid("param")
-
+		const { userId } = c.get('jwtPayload')
+		console.log(userId)
 		return c.jsonT({ message: `hello ${name}` })
 	})
