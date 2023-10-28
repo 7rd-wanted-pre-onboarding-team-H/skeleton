e7965bfe4047961a2c7a534390952191069411df
scarf <greenscarf005@gmail.com>
2023-10-28 15:12:04 +0900

    refactor: otp 생성 알고리즘 단순화

--- auth/otp.ts
+++ auth/otp.ts
@@ -1,15 +1,5 @@
-const textEncoder = new TextEncoder()
-/**
- * creates six digit otp from email
- */
-export const otpFromEmail = async (email: string) => {
-	const salt = crypto.getRandomValues(new Uint8Array(16))
-	const encoded = textEncoder.encode(`${email}${salt}${Date.now()}`)
-	const hash = await crypto.subtle.digest("SHA-256", encoded)
-	const view = new DataView(hash)
-	const high = view.getUint32(0)
-	const low = view.getUint32(4)
-	const otp = (high + low) % (10 ** 6)
+export const createOtp = () => {
+	const n = crypto.getRandomValues(new Uint32Array(1))
 
-	return `${otp}`.padStart(6, "0")
+	return `${n}`.slice(0, 6).padStart(6, "0")
 }
