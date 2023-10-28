21a409b978ca334a9b8e80534f151d1c8e1c47e6
scarf <greenscarf005@gmail.com>
2023-10-28 23:09:23 +0900

    feat: 회원가입 및 otp 인증

--- auth/signup_routes.ts
+++ auth/signup_routes.ts
@@ -15,6 +15,11 @@ const otpSchema = z.object({
 	otp: z.string().length(6).regex(/^[0-9]+$/).openapi({ example: "123456" }),
 })
 
+const otpResponse = {
+	description: "회원가입 인증용 OTP를 발송합니다.",
+	...openApiJson(otpSchema),
+}
+
 export const signUpRoute = createRoute({
 	method: "post",
 	path: "/auth/signup",
@@ -27,10 +32,7 @@ export const signUpRoute = createRoute({
 		},
 	},
 	responses: {
-		201: {
-			description: "회원가입 인증용 OTP를 발송합니다.",
-			...openApiJson(otpSchema),
-		},
+		201: otpResponse,
 		409: {
 			description: "이미 존재하는 사용자입니다.",
 			...errorJson(z.string().openapi({ example: "이미 존재하는 사용자입니다." })),
@@ -38,21 +40,39 @@ export const signUpRoute = createRoute({
 	},
 })
 
-export const verifyOtpRoute = createRoute({
+export const getOtpRoute = createRoute({
+	method: "get",
+	path: "/auth/otp",
+	tags: ["auth"],
+	summary: "이메일 인증 OTP를 발송합니다.",
+	request: {
+		headers: z.object({
+			Authorization: z.string()
+				.transform((value) => value.replace("Bearer ", "")),
+		}),
+	},
+	responses: { 200: otpResponse },
+})
+
+export const verifyEmailRoute = createRoute({
 	method: "post",
 	path: "/auth/otp",
 	tags: ["auth"],
-	summary: "회원가입 인증용 OTP를 검증합니다.",
+	summary: "이메일로 발송된 회원가입 인증 OTP를 검증합니다.",
 	request: {
 		body: {
 			description: "회원가입 인증용 OTP를 입력합니다.",
-			...openApiJson(otpSchema),
+			...openApiJson(otpSchema.merge(userSchema)),
 		},
 	},
 	responses: {
 		200: {
 			description: "회원가입이 완료되었습니다.",
 			...openApiJson(userSchema),
+		},
+		404: {
+			description: "인증정보를 찾을 수 없습니다.",
+			...errorJson(z.string().openapi({ example: "인증정보를 찾을 수 없습니다." })),
 		},
 		400: {
 			description: "인증정보가 일치하지 않습니다.",
