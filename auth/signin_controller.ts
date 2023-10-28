b290c50919cf6491241889c663bffe07ed062cdf
jhmin95 <tesla52@naver.com>
2023-10-28 16:51:52 +0900

    feat: jwt 적용. userId 만을 담는다. env 에 SECRET_KEY 를 지정해야 한다.

--- auth/signin_controller.ts
+++ auth/signin_controller.ts
@@ -3,10 +3,15 @@ import { OpenAPIHono } from "hono_zod_openapi";
 import { getUserByName } from "./signin_data.ts";
 import { DB } from "../types.ts";
 import { Kysely } from "kysely";
+import { sign } from 'hono/jwt'
+
+import { config } from "https://deno.land/x/dotenv/mod.ts"
 
 export const signInController = (db: Kysely<DB>) => {
     return new OpenAPIHono().openapi(signInUser, async (c) => {
         const { name, password } = c.req.valid("json")
+        
+        const envFileObject = config()
 
         console.log(name, password)
         const user = await getUserByName(db, name);
@@ -17,8 +22,15 @@ export const signInController = (db: Kysely<DB>) => {
         if (user.password !== password) {
             return c.jsonT({ error: "로그인 실패" }, 401);
         }
-        // jwt 로 변경 예정
-        c.res.headers.set("Set-Cookie", `access_token=${user.id, user.name};`)
+        const payload = {
+            iat: Date.now(),
+            sub: 'wanted social-feed user',
+            // 1 day
+            exp: Date.now() + 1000 * 60 * 60 * 24 * 1,
+            userId: user.id,
+        };
+        const token = await sign(payload, envFileObject.SECRET_KEY);
+        c.res.headers.set("Set-Cookie", `access_token=${token};`)
         return c.text("로그인 성공");
     });
 }
