2f31b5d841aa25b9104fc53bc8f1b558715e1c1a
scarf <greenscarf005@gmail.com>
2023-10-29 22:55:24 +0900

    feat: opengraph 이미지 표시 추가

--- swagger.ts
+++ swagger.ts
@@ -3,9 +3,9 @@ import { html } from "hono/html"
 import { OpenAPIHono } from "hono_zod_openapi"
 
 export const info = {
-	title: "Feed API",
+	title: "소셜 미디어 통합 Feed 서비스",
 	version: "0.0.0-alpha",
-	description: "소셜 미디어 통합 Feed 서비스",
+	description: "원티드 프리온보딩 백엔드 인턴십 7차 H조의 1주차 과제입니다.",
 }
 
 const id = "swagger-ui"
@@ -27,8 +27,14 @@ export const swaggerUiByUrl = (
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <title>${title}</title>
+                <meta property="og:title" content="${title}" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="https://raw.githubusercontent.com/denolib/animated-deno-logo/master/deno-rect-24fps.gif" />
+                <meta property="og:image:type" content="image/gif" />
+                <meta property="og:image:alt" content="An animated Deno logo" />
                 <meta name="description" content="${description}" />
-                <meta name="og:description" content="${description}" />
+                <meta property="og:description" content="${description}" />
+                <link rel="icon" href="https://raw.githubusercontent.com/denolib/high-res-deno-logo/master/deno_hr_circle.svg" />
                 <link rel="stylesheet" href="${swaggerUi}@${version}/swagger-ui.css" />
             </head>
             <body>
