12486f468bfc486e8a32bd8da4eaf265b747c9ca
scarf <greenscarf005@gmail.com>
2023-10-31 00:17:20 +0900

    fix: jwt 경로 검증

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { logger } from "hono/logger"
 import { prettyJSON } from "hono/pretty-json"
 import { OpenAPIHono } from "hono_zod_openapi"
-import { info, serveOpenapi, swaggerUiByUrl } from "./swagger.ts"
+import { serveOpenapi } from "./swagger.ts"
 import { helloController } from "./hello/mod.ts"
 import { postingController, postingListController, postingShareController } from "./posting/mod.ts"
 import { likesController } from "./likes/mod.ts"
@@ -11,7 +11,7 @@ import { kyselyFrom } from "./kysely_from.ts"
 import { ParseJSONResultsPlugin } from "kysely"
 import { seeded } from "./test_utils.ts"
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts"
-import { jwt } from "hono/jwt"
+import { jwt, verify } from "hono/jwt"
 import { JwtPayload } from "./auth/signin_controller.ts"
 import { emailValidation } from "./emailValidation.ts"
 
@@ -19,12 +19,22 @@ const dbPath = Deno.env.get("DB_PATH") ?? Deno.args[0] ?? "test.db"
 
 console.log("DB_PATH", dbPath)
 
-const rawDb = kyselyFrom(dbPath, { plugins: [new ParseJSONResultsPlugin()] })
+const rawDb = kyselyFrom(dbPath, { plugins: [new ParseJSONResultsPlugin()], log: ["query"] })
 const db = dbPath === ":memory:" ? await seeded(rawDb) : rawDb
 
 const authedApp = new OpenAPIHono<{ Variables: JwtPayload }>()
 	.use(
-		"^(?!\/$|\/openapi\.json$).+$",
+		"/*/*",
+		async (c, next) => {
+			const jwt = c.req.cookie("access-token") as string
+			console.log(jwt, await verify(jwt, config().SECRET_KEY))
+			await next()
+		},
+		jwt({ secret: config().SECRET_KEY, cookie: "access-token" }),
+		emailValidation,
+	)
+	.use(
+		"/summations",
 		jwt({ secret: config().SECRET_KEY, cookie: "access-token" }),
 		emailValidation,
 	)
