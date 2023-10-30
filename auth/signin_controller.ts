c4764e48723d7fc16907e2df599c7e724f10387e
scarf <greenscarf005@gmail.com>
2023-10-30 22:21:51 +0900

    fix: bcrypt로 비밀번호 검증

--- auth/signin_controller.ts
+++ auth/signin_controller.ts
@@ -5,7 +5,16 @@ import { DB } from "../types.ts"
 import { Kysely } from "kysely"
 import { sign } from "hono/jwt"
 import { setCookie } from "hono/cookie"
-import { config } from "https://deno.land/x/dotenv/mod.ts"
+import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts"
+import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts"
+
+export type JwtPayload = {
+	iat: number
+	sub: string
+	exp: number
+	userId: number
+	validated: boolean
+}
 
 export const signInController = (db: Kysely<DB>) => {
 	return new OpenAPIHono().openapi(signInUser, async (c) => {
@@ -16,18 +25,18 @@ export const signInController = (db: Kysely<DB>) => {
 		if (!user) {
 			return c.jsonT({ error: "로그인 실패. 존재하지 않는 이름" }, 401)
 		}
-		console.log(user.password, password)
-		if (user.password !== password) {
+		if (!await bcrypt.compare(password, user.password)) {
 			return c.jsonT({ error: "로그인 실패" }, 401)
 		}
-		const payload = {
+		const payload: JwtPayload = {
 			iat: Date.now() % 1000, // 1000 으로 나눠서 초 단위로 만들어줌
 			sub: "wanted social-feed user",
 			exp: Date.now() % 1000 + (60 * 60 * 24 * 1),
 			userId: user.id,
+			validated: Boolean(user.is_validated),
 		}
 		const token = await sign(payload, config().SECRET_KEY)
 		setCookie(c, "access-token", token, { path: "/", maxAge: 60 * 60 * 24 * 1 })
-		return c.text("로그인 성공")
+		return c.jsonT({ message: "로그인 성공" })
 	})
 }
