a2d9c4195a0be35a782f2efcc76b2ef2307c687d
Yonge2 <dlwodyd25@naver.com>
2023-10-30 17:36:14 +0900

    refactor: 반환 형식 요구사항에 맞게 변경, 에러 검사 추가 (#27)

--- summation/summation_data.ts
+++ summation/summation_data.ts
@@ -5,21 +5,47 @@ import type { SummationSqlOption } from "./summation_controller.ts"
 
 export const getSummation = (
 	db: Kysely<DB>,
-	{ content, dateFormat, start, end }: SummationSqlOption,
+	{ content, dateFormat, start, end, value }: SummationSqlOption,
 ) => {
-	return db.selectFrom("posting")
+	//base table
+	const base = db.selectFrom("posting")
 		.innerJoin("posting_to_hashtag as post2tag", "posting.id", "post2tag.posting_id")
 		.innerJoin("hashtag", "post2tag.hashtag_id", "hashtag.id")
 		.where(({ between }) => between("posting.created_at", start, end))
 		.where("hashtag.content", "=", content)
-		.select(({ fn: { count, sum } }) => [
+
+	//select column
+	if (value === "postings") {
+		return base.select(({ fn: { count } }) => [
 			sql<string>`strftime(${dateFormat}, posting.created_at)`.as("day"),
-			count<number>("posting.id").as("postings"),
-			sum<number>("posting.view_count").as("views"),
-			sum<number>("posting.like_count").as("likes"),
-			sum<number>("posting.share_count").as("shares"),
+			count<number>("posting.id").as("count"),
 		])
-		.groupBy(sql<string>`strftime(${dateFormat}, posting.created_at)`)
-		.orderBy(sql<string>`strftime(${dateFormat}, posting.created_at)`, "desc")
-		.execute()
+			.groupBy(sql<string>`strftime(${dateFormat}, posting.created_at)`)
+			.orderBy(sql<string>`strftime(${dateFormat}, posting.created_at)`, "desc")
+			.execute()
+	} else if (value === "views") {
+		return base.select(({ fn: { sum } }) => [
+			sql<string>`strftime(${dateFormat}, posting.created_at)`.as("day"),
+			sum<number>("posting.view_count").as("count"),
+		])
+			.groupBy(sql<string>`strftime(${dateFormat}, posting.created_at)`)
+			.orderBy(sql<string>`strftime(${dateFormat}, posting.created_at)`, "desc")
+			.execute()
+	} else if (value === "likes") {
+		return base.select(({ fn: { sum } }) => [
+			sql<string>`strftime(${dateFormat}, posting.created_at)`.as("day"),
+			sum<number>("posting.like_count").as("count"),
+		])
+			.groupBy(sql<string>`strftime(${dateFormat}, posting.created_at)`)
+			.orderBy(sql<string>`strftime(${dateFormat}, posting.created_at)`, "desc")
+			.execute()
+	} else if (value === "shares") {
+		return base.select(({ fn: { sum } }) => [
+			sql<string>`strftime(${dateFormat}, posting.created_at)`.as("day"),
+			sum<number>("posting.share_count").as("count"),
+		])
+			.groupBy(sql<string>`strftime(${dateFormat}, posting.created_at)`)
+			.orderBy(sql<string>`strftime(${dateFormat}, posting.created_at)`, "desc")
+			.execute()
+	} else throw new Error("error")
 }
